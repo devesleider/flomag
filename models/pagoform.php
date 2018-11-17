c5c15618dc9cde8738ca272472392f4bc4a4c701
devesleider <devesleider@gmail.com>
2018-11-16 21:47:49 -0500

    final

--- models/pagoform.php
+++ models/pagoform.php
@@ -1,16 +1,12 @@
 <?php defined('_JEXEC') or die;
-
 jimport('joomla.application.component.modelform');
 jimport('joomla.event.dispatcher');
-
 /**
  * Flota model.
  */
 class FlotaModelPagoForm extends JModelForm
 {
-
 	var $_item = null;
-
 	/**
 	 * Method to auto-populate the model state.
 	 *
@@ -21,7 +17,6 @@ class FlotaModelPagoForm extends JModelForm
 	protected function populateState()
 	{
 		$app = JFactory::getApplication('com_flota');
-
 		// Load state from the request userState on edit or from the passed variable on default
 		if (JFactory::getApplication()->input->get('layout') == 'edit')
 		{
@@ -33,7 +28,6 @@ class FlotaModelPagoForm extends JModelForm
 			JFactory::getApplication()->setUserState('com_flota.edit.pago.id', $id);
 		}
 		$this->setState('pago.id', $id);
-
 		// Load the parameters.
 		$params       = $app->getParams();
 		$params_array = $params->toArray();
@@ -42,9 +36,7 @@ class FlotaModelPagoForm extends JModelForm
 			$this->setState('pago.id', $params_array['item_id']);
 		}
 		$this->setState('params', $params);
-
 	}
-
 	/**
 	 * Method to get an ojbect.
 	 *
@@ -55,44 +47,33 @@ class FlotaModelPagoForm extends JModelForm
 	public function &getData($id = null){
 		if ($this->_item === null){
 			$this->_item = false;
-
 			if (empty($id)){
 				$id = $this->getState('pago.id');
 			}
-
 			// Get a level row instance.
 			$table = $this->getTable();
-
 			// Attempt to load the row.
 			if ($table !== false && $table->load($id)){
 				$user = JFactory::getUser();
 				$id   = $table->id;
-
 				// Convert the JTable to a clean JObject.
 				$properties  = $table->getProperties(1);
 				$this->_item = JArrayHelper::toObject($properties, 'JObject');
 			}
 		}
-
 		return $this->_item;
 	}
-
 	public function getTable($type = 'Pago', $prefix = 'FlotaTable', $config = array())
 	{
 		$this->addTablePath(JPATH_ADMINISTRATOR . '/components/com_flota/tables');
-
 		return JTable::getInstance($type, $prefix, $config);
 	}
-
 	public function getItemIdByAlias($alias)
 	{
 		$table = $this->getTable();
-
 		$table->load(array( 'alias' => $alias ));
-
 		return $table->id;
 	}
-
 	/**
 	 * Method to check in an item.
 	 *
@@ -105,13 +86,10 @@ class FlotaModelPagoForm extends JModelForm
 	{
 		// Get the id.
 		$id = (!empty($id)) ? $id : (int) $this->getState('pago.id');
-
 		if ($id)
 		{
-
 			// Initialise the table
 			$table = $this->getTable();
-
 			// Attempt to check the row in.
 			if (method_exists($table, 'checkin'))
 			{
@@ -121,10 +99,8 @@ class FlotaModelPagoForm extends JModelForm
 				}
 			}
 		}
-
 		return true;
 	}
-
 	/**
 	 * Method to check out an item for editing.
 	 *
@@ -137,16 +113,12 @@ class FlotaModelPagoForm extends JModelForm
 	{
 		// Get the user id.
 		$id = (!empty($id)) ? $id : (int) $this->getState('pago.id');
-
 		if ($id)
 		{
-
 			// Initialise the table
 			$table = $this->getTable();
-
 			// Get the current user object.
 			$user = JFactory::getUser();
-
 			// Attempt to check the row out.
 			if (method_exists($table, 'checkout'))
 			{
@@ -156,10 +128,8 @@ class FlotaModelPagoForm extends JModelForm
 				}
 			}
 		}
-
 		return true;
 	}
-
 	/**
 	 * Method to get the profile form.
 	 *
@@ -182,10 +152,8 @@ class FlotaModelPagoForm extends JModelForm
 		{
 			return false;
 		}
-
 		return $form;
 	}
-
 	/**
 	 * Method to get the data that should be injected in the form.
 	 *
@@ -200,10 +168,8 @@ class FlotaModelPagoForm extends JModelForm
 			$data = $this->getData();
 		}
 		
-
 		return $data;
 	}
-
 	/**
 	 * Method to save the form data.
 	 *
@@ -235,9 +201,7 @@ class FlotaModelPagoForm extends JModelForm
 			print_r("no se pudo");
 			return false;
 		}
-
 	}
-
 	public function delete($data)
 	{
 		$id = (!empty($data['id'])) ? $data['id'] : (int) $this->getState('pago.id');
@@ -256,45 +220,37 @@ class FlotaModelPagoForm extends JModelForm
 		}
 	}
 	
-	/*public function getURL(){
+	public function getURL(){
 		return "https://secure.placetopay.com/redirection/api/session/";
 	}
-
 	public function getlogin(){
 		return "180c674b40b47a241a5f3372115575cd";
 	}
-
 	public function gettrankey(){
 		return "3cTH56pOprppMef3";
-	}*/
-
+	}
+	/*Credenciales de pruebas.
 	public function getURL(){
 		return "https://test.placetopay.com/redirection/api/session/";
 	}
-
 	public function getlogin(){
 		return "bb64e391a1980021bce85e8b022e82af";
 	}
-
 	public function gettrankey(){
 		return "Tu138rtLze59PC98";
-	}
-
+	}*/
 	public function getCanSave(){
 		$table = $this->getTable();
 		return $table !== false;
 	}
-
 	public function getPagos($id = null){
 		$table = $this->getTable();
 		return $table->getPagos($id);
 	}
-
 	public function getPendientes($id = null){
 		$table = $this->getTable();
 		return $table->getPendientes($id);
 	}
-
 	public function getPago($pid=null){
 		if($pid!=null){
 			$table = $this->getTable();
@@ -302,17 +258,12 @@ class FlotaModelPagoForm extends JModelForm
 		}
 		return null;
 	}
-
 	public function getDescuento($bono=null, $fecha=null){
 		if($bono!=null && $fecha!=null){
 			$table = $this->getTable();
 			return $table->getDescuento($bono,$fecha);
 		}
 		return null;
-
 	}
-
-
 	
-
 }
